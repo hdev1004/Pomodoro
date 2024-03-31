3060e914e0e784401872072ddef16768b5e7b48b
hdev1004 <h.dev@kakao.com>
2024-03-31 23:52:08 +0900

    feat: 다이얼 추가

--- Pomodoro/MainWindow.xaml.cs
+++ Pomodoro/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,8 +8,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Pomodoro
 {
@@ -16,18 +20,142 @@ namespace Pomodoro
     /// </summary>
     public partial class MainWindow : Window
     {
-        private void DrawPie()
+        private double scale = 0.5;
+        private static int width = 380;
+        private static int height = 380;
+        private static int pomodoro_width = 340;
+        private static int pomodoro_height = 340;
+        static double second = 360.0;
+        private DateTime startTime;
+        private ScaleTransform transform;
+        ScaleTransform scaleTransform;
+
+        double nowTime = 0;
+        private void timer_Tick(Object sender, EventArgs e)
         {
+            TimeSpan elapsedTime = DateTime.Now - startTime;
+            nowTime = elapsedTime.TotalSeconds;
+
+            if (nowTime < second)
+            {
+                nowTime = nowTime / second * 2;
+            } else
+            {
+                nowTime = 2.0;
+
+            }
+            DrawPie(nowTime);
+
 
         }
 
+        private void DrawPie(double time)
+        {
+            double size = 270 * scale - 20;
+            canvas.Children.Clear();
+            Rect rect = new Rect(0, 0, size, size);
+            Thickness margin = new Thickness((this.Height / 2) - (size / 2));
+            canvas.Margin = margin;
+
+            Console.WriteLine((this.Height / 2) - (size / 2));
+
+            Point point1;
+            Point point2;
+
+            RotateTransform rotateTransform = new RotateTransform(time * 180);
+            Middle.RenderTransform = rotateTransform;
+            
+
+            if(time <= 1.0)
+            {
+                Path pieSlice1 = this.canvas.AddPieSlice
+                (
+                 new SolidColorBrush(Color.FromArgb(255, (byte)139, (byte)155, (byte)197)),
+                    new SolidColorBrush(Color.FromArgb(255, (byte)139, (byte)155, (byte)197)),
+                    1,
+                    rect,
+                    0,
+                    time * Math.PI,
+                    false,
+                    SweepDirection.Clockwise,
+                    out point1,
+                    out point2
+                );
+
+                pieSlice1.StrokeLineJoin = PenLineJoin.Round;
+            } else 
+            {
+                Path pieSlice1 = this.canvas.AddPieSlice
+                (
+                 new SolidColorBrush(Color.FromArgb(255, (byte)139, (byte)155, (byte)197)),
+                  new SolidColorBrush(Color.FromArgb(255, (byte)139, (byte)155, (byte)197)),
+                   1,
+                   rect,
+                   0,
+                   1 * Math.PI,
+                   false,
+                   SweepDirection.Clockwise,
+                   out point1,
+                   out point2
+                );
+                pieSlice1.StrokeLineJoin = PenLineJoin.Round;
+
+                Path pieSlice2 = this.canvas.AddPieSlice
+                (
+                 new SolidColorBrush(Color.FromArgb(255, (byte)139, (byte)155, (byte)197)),
+                    new SolidColorBrush(Color.FromArgb(255, (byte)139, (byte)155, (byte)197)),
+                    1,
+                    rect,
+                    1 * Math.PI,
+                    time * Math.PI,
+                    false,
+                    SweepDirection.Clockwise,
+                    out point1,
+                    out point2
+                );
+
+                pieSlice2.StrokeLineJoin = PenLineJoin.Round;
+            }
+
+           
+
+            
+        }
+
+        private void Resize()
+        {
+            this.Width = width * scale;
+            this.Height = height * scale;
+            Rect_Form.Width = width * scale - 10;
+            Rect_Form.Height = height * scale - 10;
+
+
+            Pomodoro.Width = pomodoro_width * scale;
+            Pomodoro.Height = pomodoro_height * scale - 50;
+
+            Middle.Width = 45 * scale;
+            Middle.Height = 45 * scale;
+            Middle_Stick.Height = 22.5 * scale;
+        }
         private void Loaded()
         {
-            DrawPie();
+            
+            startTime = DateTime.Now;
+            DispatcherTimer timer = new DispatcherTimer();    //객체생성
+            timer.Interval = TimeSpan.FromMilliseconds(1);    //시간간격 설정
+            timer.Tick += new EventHandler(timer_Tick);          //이벤트 추가
+            timer.Start();
+            /*
+                        scaleTransform = new ScaleTransform(scale, scale, width * scale / 2, height * scale / 2);
+                        Pomodoro_Form.RenderTransform = scaleTransform;*/
+
+            Resize();
         }
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+
+
             Loaded();
         }
 
@@ -37,9 +165,29 @@ namespace Pomodoro
         }
 
 
-        private void Close_Click(object sender, RoutedEventArgs e)
+
+        private void Close_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             this.Close();
+
+        }
+
+        private void ScaleSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double sliderValue = e.NewValue;
+            Console.WriteLine(sliderValue);
+            //scale = 0.5;
+
+            this.Width = width * scale;
+            this.Height = height * scale;
+            Rect_Form.Width = width * scale - 10;
+            Rect_Form.Height = height * scale - 10;
+
+
+            Pomodoro.Width = pomodoro_width * scale;
+            Pomodoro.Height = pomodoro_height * scale - 50;
+
+
         }
     }
 }
